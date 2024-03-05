57c9845b9d195a42dd46eb872d8c23afd2f6eb70
Alitryel <alitryel@uandex.ru>
2024-03-05 19:10:05 +0300

    Init 2.0

--- src/main/java/ru/alitryel/bfmetvennorath/repositories/GameRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.alitryel.bfmetvennorath.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.alitryel.bfmetvennorath.entities.Game;
-
-public interface GameRepository extends JpaRepository<Game, Long> {
-}
--- src/main/java/ru/alitryel/bfmetvennorath/repositories/SearchRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.alitryel.bfmetvennorath.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.alitryel.bfmetvennorath.entities.Search;
-
-
-public interface SearchRepository extends JpaRepository<Search, Long> {
-
-}
